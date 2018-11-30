f755c5b0d312eecafeb4ef5a4f89ab437e5a0f0e
frank.gitonga <frank.gitonga@incentro.com>
2018-11-30 14:51:47 +0300

    contentfull

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,21 @@
+require(`dotenv`).config({path: `.env`,})
+
 module.exports = {
   siteMetadata: {
-    name: `Hello Typescript World!`,
-    tagline: `Gatsby + SASS + Typescript = 💪`
-  },  
+    name: `Star Wars Movies 🎥`,
+    tagline: `"Do, or do not. There is no try."`,
+    githubLink: `https://github.com/tdharmon/gatsby-example-typescript-sass-contentful`,
+  }, 
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-tslint`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,// space id here
+        accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN // access token here
+      },
+    },
   ],
 }
